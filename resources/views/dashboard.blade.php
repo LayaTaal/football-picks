7003ad686d6c92ced3dd7837b38ff20451a5983a
Jason Zinn <jason@zinnfinity.com>
2022-09-17 16:11:23 -0400

    temporarily hid survivor table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,6 +21,7 @@
         <x-season-stats />
     </x-section>
 
+    <!--
     <div class="pt-12 pb-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 bg-white border-b border-gray-200 overflow-scroll shadow-sm sm:rounded-lg">
@@ -29,5 +30,6 @@
             </div>
         </div>
     </div>
+    -->
 
 </x-app-layout>
