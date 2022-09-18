124020593fb0d6cca091ded6363f285aa034e6f9
Jason Zinn <jason@zinnfinity.com>
2022-09-18 08:37:25 -0400

    fixed bug with survivor pick not existing in post array

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -60,7 +60,7 @@ class DashboardController extends Controller {
             }
         }
 
-        if ( $games_data['survivor_pick'] !== null ) {
+        if ( array_key_exists( 'survivor_pick', $games_data ) && $games_data['survivor_pick'] !== null ) {
             $survivor_game  = Game::where( 'season_id', config( 'settings' )['active_season'] )
                                   ->where( 'round_id', config( 'settings' )['active_round'] )
                                   ->where( 'home_team', $games_data['survivor_pick'] )
