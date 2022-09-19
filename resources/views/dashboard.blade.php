9f976acd50df39f9afc4d85c24303e965de157ba
Jason Zinn <jason@zinnfinity.com>
2022-09-18 20:37:03 -0400

    removed survivor stats table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,12 +21,12 @@
         <x-season-stats />
     </x-section>
 
-    <!--
+    <!-- Survivor Stats will go here
     <div class="pt-12 pb-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 bg-white border-b border-gray-200 overflow-scroll shadow-sm sm:rounded-lg">
                 <h2 class="text-2xl mb-6">Survivor Picks</h2>
-                <x-survivor-stats />
+
             </div>
         </div>
     </div>
