559979c2eb70810077082923e7148ce9fe150ddb
Jason Zinn <jason@zinnfinity.com>
2022-09-21 08:49:40 -0400

    removed duplicate section from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,10 +21,6 @@
         <x-season-stats />
     </x-section>
 
-    <x-section title="Survivor Picks">
-        <x-season-stats />
-    </x-section>
-
     <div class="pt-12 pb-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 bg-white border-b border-gray-200 overflow-scroll shadow-sm sm:rounded-lg">
