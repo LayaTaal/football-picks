3832188036f38df06be5874964f9cee4658154a9
LayaTaal <jason@zinnfinity.com>
2022-11-06 09:54:59 -0500

    cleaend up code

--- app/Http/Controllers/AdminSettingController.php
+++ app/Http/Controllers/AdminSettingController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Round;
 use App\Models\Season;
 use App\Models\Setting;
-use Illuminate\Http\Request;
 
 class AdminSettingController extends Controller {
 
@@ -27,37 +26,27 @@ class AdminSettingController extends Controller {
     }
 
     public function update() {
-        var_dump( 'Updating settings' );
-        die;
-        dd( $request->all() );
         $active_season         = Setting::where( 'name', 'active_season' )->first();
         $active_round          = Setting::where( 'name', 'active_round' )->first();
         $daylight_savings_time = Setting::where( 'name', 'daylight_savings_time' )->first();
 
-        if ( $request->input( 'active_season' ) ) {
-            $active_season->value = $request->input( 'active_season' );
+        if ( request()->input( 'active_season' ) ) {
+            $active_season->value = request()->input( 'active_season' );
             $active_season->save();
         }
 
-        if ( $request->input( 'active_round' ) ) {
-            $active_round->value = $request->input( 'active_round' );
+        if ( request()->input( 'active_round' ) ) {
+            $active_round->value = request()->input( 'active_round' );
             $active_round->save();
         }
 
-        dd(
-            [
-                'model'   => $daylight_savings_time,
-                'request' => $request->input( 'daylight_savings_time' ),
-            ]
-        );
-
         if ( $daylight_savings_time ) {
-            $daylight_savings_time->value = $request->input( 'daylight_savings_time' ) === "true" ? 1 : 0;
+            $daylight_savings_time->value = request()->input( 'daylight_savings_time' ) === "true" ? 1 : 0;
             $daylight_savings_time->save();
         } else {
             $setting        = new Setting;
             $setting->name  = "daylight_savings_time";
-            $setting->value = $request->input( 'daylight_savings_time' ) === "true" ? 1 : 0;
+            $setting->value = request()->input( 'daylight_savings_time' ) === "true" ? 1 : 0;
             $setting->save();
         }
 
